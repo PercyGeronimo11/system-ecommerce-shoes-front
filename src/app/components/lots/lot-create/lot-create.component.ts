a9fe013c54fe5707ac413f7aa037edb0b06d86b0
PercyGeronimo <percy.geronimo.d@gmail.com>
2024-06-28 11:59:19 -0500

    primeros componentes

---
+++ src/app/components/lots/lot-create/lot-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lot-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './lot-create.component.html',
+  styleUrl: './lot-create.component.scss'
+})
+export class LotCreateComponent {
+
+}
