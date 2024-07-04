4c70e0b51894218e76428233cc2cc9b8130cbc2a
Jack Portilla <jackportilla24@gmail.com>
2024-07-04 00:22:25 -0500

    arreglo

--- src/app/components/ecommerce/ecommerce.component.ts
+++ src/app/components/ecommerce/ecommerce.component.ts
@@ -1,31 +1,33 @@
-import { CommonModule } from '@angular/common'; // Importa CommonModule para directivas comunes de Angular
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Product } from '../../.../../models/product/product.model';
 import { ProductService } from '../../.../../services/products/product.service';
-
 
 @Component({
   selector: 'app-ecommerce',
   standalone: true,
-  imports: [CommonModule], // Asegúrate de importar los módulos necesarios
+  imports: [CommonModule],
   templateUrl: './ecommerce.component.html',
   styleUrls: ['./ecommerce.component.scss']
 })
-
-export class EcommerceComponent {
-
-
-  constructor(private productService: ProductService) {}
-
-
+export class EcommerceComponent implements OnInit, OnDestroy {
   products: Product[] = [];
   isLoading = false;
   error: string | null = null;
-  
+
+  slides = [
+    { image: 'assets/images/baner1.jpg', caption: '', description: '' },
+    { image: 'assets/images/baner2.jpg', caption: '', description: '' },
+  ];
+  currentSlide = 0;
+  slideInterval: any;
+
+  constructor(private productService: ProductService) {}
+
   ngOnInit() {
     this.isLoading = true;
     this.productService.getProducts()
-      .subscribe((products: any )=> {
+      .subscribe((products: any) => {
         this.products = products.data.content;
         this.isLoading = false;
         console.log("data:", products);
@@ -34,11 +36,31 @@ export class EcommerceComponent {
         this.isLoading = false;
         console.log("error:", error);
       });
+
+    this.startAutoSlide();
   }
+
+  ngOnDestroy() {
+    clearInterval(this.slideInterval);
+  }
+
   addToCart(product: any): void {
     console.log('Producto agregado al carrito', product);
-  } 
+  }
 
+  previousSlide(event: Event) {
+    event.preventDefault();
+    this.currentSlide = (this.currentSlide === 0) ? this.slides.length - 1 : this.currentSlide - 1;
+  }
 
-  
+  nextSlide(event: Event) {
+    event.preventDefault();
+    this.currentSlide = (this.currentSlide === this.slides.length - 1) ? 0 : this.currentSlide + 1;
+  }
+
+  startAutoSlide() {
+    this.slideInterval = setInterval(() => {
+      this.currentSlide = (this.currentSlide === this.slides.length - 1) ? 0 : this.currentSlide + 1;
+    }, 3000); // Cambia la imagen cada 5 segundos
+  }
 }
