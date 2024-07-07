8371ed758d504984e315f2fc29c32cb013c0966f
alfredo7792 <alfredo7791@gmail.com>
2024-07-06 23:29:40 -0500

    update materials

--- src/app/components/materials/materials-list/materials-list.component.ts
+++ src/app/components/materials/materials-list/materials-list.component.ts
@@ -6,6 +6,7 @@ import { SharedModule } from 'src/app/theme/shared/shared.module';
 import { MaterialService } from '../service/materials.service';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tbl-bootstrap',
@@ -18,19 +19,30 @@ export class MaterialsListModule implements OnInit {
   materials: any = [];
   modalDeleteVisible: boolean = false;
   selectedMaterial: any = null;
+  isEditMode = false;
+  materialForm: FormGroup;
+  modalMaterialVisible = false;
   constructor(
     public materialService: MaterialService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder,
   ) {
+    this.materialForm = this.fb.group({
+      name: ['', [Validators.required]],
+      stock: ['', [Validators.required]],
+      unitPrice: ['', [Validators.required]],
+      unit: ['', [Validators.required]],
+      description: ['', []],
+    });
   }
 
   ngOnInit(): void {
     this.listMaterials();
   }
 
-  selectMaterial(material:any):void{
+  /* selectMaterial(material:any):void{
     this.router.navigate(['/materialEdit', material.id]);
-  }
+  } */
 
   listMaterials() {
     this.materialService.list().subscribe((resp:any) => {
@@ -62,4 +74,58 @@ export class MaterialsListModule implements OnInit {
     }
   }
 
+  selectMaterial(material: any) {
+    this.isEditMode = true;
+    this.selectedMaterial = material;
+    this.materialForm.patchValue({
+      name: material.name,
+      stock: material.stock,
+      unitPrice: material.unitPrice,
+      unit: material.unit,
+      description: material.descripcion
+    });
+    this.modalMaterialVisible = true;
+  }
+
+  openMaterialModal() {
+    this.isEditMode = false;
+    this.materialForm.reset({
+      unit: 'm'
+    });
+    this.modalMaterialVisible = true;
+  }
+
+  closeMaterialModal() {
+    this.modalMaterialVisible = false;
+  }
+
+  onSubmitMaterial() {
+    if (this.materialForm.invalid) {
+      this.materialForm.markAllAsTouched();
+      return;
+    }
+
+    const user = {
+      ...this.materialForm.value,
+      id: this.isEditMode ? this.selectedMaterial?.id : undefined
+    };
+
+    if (this.isEditMode) {
+      this.materialService.edit(user.id, user).subscribe(() => {
+        console.log('Material actualizado exitosamente!');
+        this.listMaterials();
+        this.closeMaterialModal();
+      }, error => {
+        console.error('Error actualizando el material', error);
+      });
+    } else {
+      this.materialService.create(user).subscribe(() => {
+        console.log('Material creado exitosamente!');
+        this.listMaterials();
+        this.closeMaterialModal();
+      }, error => {
+        console.error('Error creando el material', error);
+      });
+    }
+  }
 }
