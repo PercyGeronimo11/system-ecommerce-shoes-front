97971c0509036f2dfe9de552e6c3c9e2e7cfa08a
alfredo7792 <124851707+alfredo7792@users.noreply.github.com>
2024-08-19 14:54:12 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://127.0.0.1:8080'
+  apiUrl: 'http://http://157.230.191.218:8080',
+  apiUrlMl: 'http://http://157.230.191.218:5000'
 };
